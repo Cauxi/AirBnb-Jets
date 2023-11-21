d8be581a05cdfa03bfd291cac8b265fcb4467206
Carlos Coelho <carlos.neto.coelho@outlook.pt>
2023-11-21 16:50:00 +0000

    made jet validations, and changed new jet form

--- app/models/jet.rb
+++ app/models/jet.rb
@@ -1,4 +1,7 @@
 class Jet < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  validates :name, presence: true
+  validates :description, presence: true, length: { minimum: 10 }
+  validates :price, presence: true
 end
