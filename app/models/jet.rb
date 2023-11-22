d834bab5e0cd7972e93554a36ef720dd4f558f9d
Carlos Coelho <carlos.neto.coelho@outlook.pt>
2023-11-22 12:40:03 +0000

    added map with markers

--- app/models/jet.rb
+++ app/models/jet.rb
@@ -6,6 +6,7 @@ class Jet < ApplicationRecord
   has_one_attached :photo
   validates :name, presence: true
   validates :description, presence: true, length: { minimum: 10 }
+  validates :city, presence: true
   validates :price, presence: true, numericality: { only_integer: true }
   validates :photo, presence:true
 end
