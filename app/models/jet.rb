543530fdbf6eba6d9779109388d4e783bd51a04b
Carlos Coelho <carlos.neto.coelho@outlook.pt>
2023-11-22 11:38:11 +0000

    geocode is funcional

--- app/models/jet.rb
+++ app/models/jet.rb
@@ -1,5 +1,7 @@
 class Jet < ApplicationRecord
   belongs_to :user
+  geocoded_by :city
+  after_validation :geocode, if: :will_save_change_to_city?
   has_many :bookings
   has_one_attached :photo
   validates :name, presence: true
