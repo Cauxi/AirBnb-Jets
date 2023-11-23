3926780f210a78f887be4383c2bc67a7e359d032
Tiago Neto <tiagocastroneto@yahoo.com>
2023-11-23 15:16:38 +0000

    done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,13 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
-    count = 0
+    if current_user
+      count = 0
 
-    current_user.jets.each do |jet|
-      count += jet.bookings.count { |booking| booking.status == "pending" }
-    end
-    @bookings_pending = count
+      current_user.jets.each do |jet|
+        count += jet.bookings.count { |booking| booking.status == "pending" }
+      end
+      @bookings_pending = count
+    end 
   end
 end
